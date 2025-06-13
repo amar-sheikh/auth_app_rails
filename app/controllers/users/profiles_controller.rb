0fad5c2a0dc1d46104d9ab9dd8992f2298f410a2
umar <umar.m.sheikh@gmail.com>
2025-06-13 15:28:48 +0500

    Allow edit email and password independently

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -8,11 +8,17 @@ class Users::ProfilesController < ApplicationController
   def update
     email_changed = @user.email != user_params[:email]
 
-    if @user.update_with_password(user_params)
+    successfully_updated = if user_params[:password].blank?
+                             @user.update_without_password(user_params.slice(:email))
+                           else
+                             @user.update_with_password(user_params)
+                           end
+
+
+    if successfully_updated
       if email_changed
+        @user.update_columns(confirmed_at: nil)
         @user.send_confirmation_instructions
-        @user.confirmed_at = nil
-        @user.save(validate: false)
       end
 
       redirect_to root_path, notice: "Profile updated successfully."
